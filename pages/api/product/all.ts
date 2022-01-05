be6c839039044f5141e0e0f8d4844405fb7277d7
Jesdakorn Saelor <jesdakorns@gmail.com>
2022-01-06 02:42:58 +0700

    6/1/65

--- pages/api/product/all.ts
+++ pages/api/product/all.ts
@@ -18,65 +18,65 @@ const cors = {
 const get = async (req: NextApiRequest, res: NextApiResponse) => {
     let __res = {}
     await runMiddleware(req, res, cors)
-    // try {
+    try {
 
-    let category = req.query.category || ""
-    let minPrice = req.query.minPrice || ""
-    let maxPrice = req.query.maxPrice || ""
-    let keyword = req.query.keyword || ""
-    let sortBy = req.query.sortBy || ""
-    let order = req.query.order || ""
-    let page = req.query.page || 1
+        let category = req.query.category || ""
+        let minPrice = req.query.minPrice || ""
+        let maxPrice = req.query.maxPrice || ""
+        let keyword = req.query.keyword || ""
+        let sortBy = req.query.sortBy || ""
+        let order = req.query.order || ""
+        let page = req.query.page || 1
 
-    console.log(`minPrice`, minPrice)
-    console.log(`maxPrice`, maxPrice)
+        console.log(`minPrice`, minPrice)
+        console.log(`maxPrice`, maxPrice)
 
-    // console.log(`category`, typeof category, category)
-    // console.log(`minPrice`, typeof minPrice, minPrice)
-    // console.log(`maxPrice`, typeof maxPrice, maxPrice)
-    // console.log(`keyword`, typeof keyword, keyword)
-    // console.log(`sortBy`, typeof sortBy, sortBy)
-    // console.log(`order`, typeof order, order)
-    // console.log(`page`, typeof page, page)
+        // console.log(`category`, typeof category, category)
+        // console.log(`minPrice`, typeof minPrice, minPrice)
+        // console.log(`maxPrice`, typeof maxPrice, maxPrice)
+        // console.log(`keyword`, typeof keyword, keyword)
+        // console.log(`sortBy`, typeof sortBy, sortBy)
+        // console.log(`order`, typeof order, order)
+        // console.log(`page`, typeof page, page)
 
-    let sqlKeyword = ``
-    let sqlCategory = ``
-    let sqlPrice = ``
-    let sqlSortBy = ``
-    if (sortBy === '') {
-        sqlSortBy = ` order by p.id desc `
-    } else if (sortBy === "price") {
-        console.log(`price`)
-        sqlSortBy = ` order by p.price ${order} `
-    } else if (sortBy === "sales") {
-        console.log(`sales`);
-        sqlSortBy = ` order by p.price desc `
-    }
+        let sqlKeyword = ``
+        let sqlCategory = ``
+        let sqlPrice = ``
+        let sqlSortBy = ``
+        if (sortBy === '') {
+            sqlSortBy = ` order by p.id desc `
+        } else if (sortBy === "price") {
+            console.log(`price`)
+            sqlSortBy = ` order by p.price ${order} `
+        } else if (sortBy === "sales") {
+            console.log(`sales`);
+            sqlSortBy = ` order by p.price desc `
+        }
 
-    if (minPrice != "" && maxPrice != "") {
-        sqlPrice = (minPrice != "" && maxPrice != "" ? ` and p.price between ${minPrice} AND ${maxPrice} ` : ``)
-    } else if (minPrice != "") {
-        sqlPrice = (minPrice != "" ? ` and p.price <= ${minPrice} ` : ``)
-    } else if (maxPrice != "") {
-        sqlPrice = (maxPrice != "" ? ` and p.price >= ${maxPrice} ` : ``)
-    }
-
-
-    sqlKeyword = (keyword != "" ? ` and (p.title like "%${keyword}%" or pty.title_th like "%${keyword}%" or pty.title_en like "%${keyword}%")  ` : ``)
-    sqlCategory = (category != "" ? ` and  (pty.title_th like "%${category}%" or pty.title_en like "%${category}%") ` : ``)
+        if (minPrice != "" && maxPrice != "") {
+            sqlPrice = (minPrice != "" && maxPrice != "" ? ` and p.price between ${minPrice} AND ${maxPrice} ` : ``)
+        } else if (minPrice != "") {
+            sqlPrice = (minPrice != "" ? ` and p.price <= ${minPrice} ` : ``)
+        } else if (maxPrice != "") {
+            sqlPrice = (maxPrice != "" ? ` and p.price >= ${maxPrice} ` : ``)
+        }
 
 
-    let per_page = 15;
-    let offset = (Number(page) - 1) * per_page;
-
-    let rowProduct = await db.query(`select count(*) as rows from products where remove = "false"`)
+        sqlKeyword = (keyword != "" ? ` and (p.title like "%${keyword}%" or pty.title_th like "%${keyword}%" or pty.title_en like "%${keyword}%")  ` : ``)
+        sqlCategory = (category != "" ? ` and  (pty.title_th like "%${category}%" or pty.title_en like "%${category}%") ` : ``)
 
 
-    let last_page = Math.ceil(rowProduct[0].rows / per_page)
-    console.log(`page`, typeof last_page, last_page)
+        let per_page = 15;
+        let offset = (Number(page) - 1) * per_page;
+
+        let rowProduct = await db.query(`select count(*) as rows from products where remove = "false"`)
 
 
-    let textSqlProducts = `
+        let last_page = Math.ceil(rowProduct[0].rows / per_page)
+        console.log(`page`, typeof last_page, last_page)
+
+
+        let textSqlProducts = `
         select 
             p.id as product_id ,
             p.title as product_title,
@@ -100,58 +100,58 @@ const get = async (req: NextApiRequest, res: NextApiResponse) => {
         join product_cover_image as pci on pci.product_id = p.id
         join province as pv on pv.id = p.address
         where p.remove = "false" 
-        ${sqlKeyword}${sqlCategory}${sqlSortBy}${sqlPrice}
+        ${sqlKeyword}${sqlCategory}${sqlPrice}${sqlSortBy}
 
         limit ${offset},${per_page}`
-    console.log(`textSqlProducts`, textSqlProducts)
-    let getProducts = await db.query(
-        textSqlProducts
-    )
+        console.log(`textSqlProducts`, textSqlProducts)
+        let getProducts = await db.query(
+            textSqlProducts
+        )
 
 
-    if (getProducts.length == 0) {
+        if (getProducts.length == 0) {
+            __res = {
+                status: {
+                    error: false,
+                    message: 'not data'
+                },
+                timestamp: Math.floor(Date.now() / 1000)
+            }
+            res.status(200).json(__res)
+            return;
+        }
+
+
+
         __res = {
             status: {
                 error: false,
-                message: 'not data'
+                message: ''
             },
+            data: getProducts,
+            // total: rowProduct[0].rows,
+            per_page: per_page,
+            current_page: Number(page),
+            last_page: last_page,
+            path: `${process.env.NEXT_PUBLIC_APP_URL}`,
+            first_page_url: `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=1`,
+            last_page_url: `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=${last_page}`,
+            next_page_url: (Number(page) < last_page ? `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=${Number(page) + 1}` : ""),
+            prev_page_url: (Number(page) !== 1 ? `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=${Number(page) - 1}` : ""),
             timestamp: Math.floor(Date.now() / 1000)
         }
         res.status(200).json(__res)
-        return;
+
+    } catch (err) {
+        let __res = {
+            status: {
+                error: true,
+                message: 'Get product all error.'
+            },
+            timestamp: Math.floor(Date.now() / 1000)
+        }
+        res.status(500).json(__res)
     }
-
-
-
-    __res = {
-        status: {
-            error: false,
-            message: ''
-        },
-        data: getProducts,
-        total: rowProduct[0].rows,
-        per_page: per_page,
-        current_page: Number(page),
-        last_page: last_page,
-        path: `${process.env.NEXT_PUBLIC_APP_URL}`,
-        first_page_url: `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=1`,
-        last_page_url: `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=${last_page}`,
-        next_page_url: (Number(page) < last_page ? `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=${Number(page) + 1}` : ""),
-        prev_page_url: (Number(page) !== 1 ? `${process.env.NEXT_PUBLIC_APP_URL}/api/product/all?page=${Number(page) - 1}` : ""),
-        timestamp: Math.floor(Date.now() / 1000)
-    }
-    res.status(200).json(__res)
-
-    // } catch (err) {
-    //     let __res = {
-    //         status: {
-    //             error: true,
-    //             message: 'Get product all error.'
-    //         },
-    //         timestamp: Math.floor(Date.now() / 1000)
-    //     }
-    //     res.status(500).json(__res)
-    // }
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
---
+++ pages/api/product/type.ts
@@ -0,0 +1,78 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import middleware from '@middleware'
+const db = require('@database');
+import { runMiddleware } from '@lib/cors'
+
+type Data = {
+    status: {
+        success: boolean,
+        message: string,
+    },
+    data?: object,
+    timestamp: number
+
+}
+const cors = {
+    methods: ['GET'],
+}
+const get = async (req: NextApiRequest, res: NextApiResponse) => {
+    let __res = {}
+    await runMiddleware(req, res, cors)
+    // try {
+    let textTypeProduct = `
+    select 
+        pty.id as product_type_id,
+        pty.title_en as product_type_title_en,
+        pty.title_th as product_type_title_th,
+        pty.remove as product_type_remove
+    from product_type as pty
+    where pty.remove = "false"`
+    let getTypeProduct = await db.query(textTypeProduct)
+
+    if (getTypeProduct.length == 0) {
+        __res = {
+            status: {
+                error: false,
+                message: 'not data'
+            },
+            timestamp: Math.floor(Date.now() / 1000)
+        }
+        res.status(200).json(__res)
+        return;
+    }
+
+
+
+    __res = {
+        status: {
+            error: false,
+            message: ''
+        },
+        data: getTypeProduct,
+        timestamp: Math.floor(Date.now() / 1000)
+    }
+    res.status(200).json(__res)
+
+    // } catch (err) {
+    //     let __res = {
+    //         status: {
+    //             error: true,
+    //             message: 'Get product all error.'
+    //         },
+    //         timestamp: Math.floor(Date.now() / 1000)
+    //     }
+    //     res.status(500).json(__res)
+    // }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    switch (req.method) {
+        case 'GET':
+            get(req, res)
+            break
+        default:
+            res.status(405).end() //Method Not Allowed
+            break
+    }
+
+}
